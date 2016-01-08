63949712510d32367d51f387ddc5ea2cdf249203
Gaston Jorquera <gjorquera@gmail.com>
2016-01-08 15:17:31 -0300

    Clean up dummy creation in specs

--- spec/annotations_spec.rb
+++ spec/annotations_spec.rb
@@ -3,23 +3,15 @@ require 'spec_helper'
 class CoAspects::ExistingAspect; end
 
 describe CoAspects::Annotations do
+  before { class Dummy; extend CoAspects::Annotations; end }
+
   describe 'annotations' do
     it 'should not raise an error when the aspect exists' do
-      expect {
-        class Dummy
-          extend CoAspects::Annotations
-          _existing
-        end
-      }.not_to raise_error
+      expect { class Dummy; _existing; end }.not_to raise_error
     end
 
     it 'should raise an error when the aspect does not exist' do
-      expect {
-        class Dummy
-          extend CoAspects::Annotations
-          _inexisting
-        end
-      }.to raise_error(
+      expect { class Dummy; _inexisting; end }.to raise_error(
         CoAspects::AspectNotFoundError, /InexistingAspect.*_inexisting/)
     end
   end
