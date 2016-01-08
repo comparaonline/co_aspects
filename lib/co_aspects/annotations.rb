640e89ab6638ef73e3c179e17477b01b266d4bca
Gaston Jorquera <gjorquera@gmail.com>
2016-01-08 14:57:55 -0300

    Better way of handling pending aspect attachments

--- lib/co_aspects/annotations.rb
+++ lib/co_aspects/annotations.rb
@@ -4,22 +4,26 @@ module CoAspects
   module Annotations
     def method_missing(method_name, *args, &block)
       return super unless /\A_/ =~ method_name
-      aspect_name = aspect_name_from_method(method_name)
-      store_pending_aspect(aspect_name, method_name)
+      aspect_name = __aspect_name_from_method(method_name)
+      __store_pending_aspect(aspect_name, method_name)
     end
 
     private
 
-    def store_pending_aspect(aspect_name, annotation)
+    def __aspect_name_from_method(annotation)
+      "::CoAspects::#{annotation[1..-1].camelize}Aspect"
+    end
+
+    def __store_pending_aspect(aspect_name, annotation)
       aspect = aspect_name.constantize
-      (@__pending_aspects__ ||= []) << [aspect, args, block]
+      __pending_aspects << AspectCall.new(aspect, args, block)
     rescue NameError => e
       fail AspectNotFoundError,
         "Aspect #{aspect_name} for annotation #{annotation} does not exist."
     end
 
-    def aspect_name_from_method(annotation)
-      "::CoAspects::#{annotation[1..-1].camelize}Aspect"
+    def __pending_aspects
+      @__pending_aspects__ ||= []
     end
   end
 end
