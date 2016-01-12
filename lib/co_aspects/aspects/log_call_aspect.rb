5499979f70ffb9be08e7a582d02567f3d89f565d
Gaston Jorquera <gjorquera@gmail.com>
2016-01-12 18:40:54 -0300

    Implement log aspect

--- lib/co_aspects/aspects/log_call_aspect.rb
+++ lib/co_aspects/aspects/log_call_aspect.rb
@@ -1,6 +1,13 @@
 module CoAspects
   module Aspects
     class LogCallAspect < Aspector::Base
+      around method_arg: true do |method, proxy, *args, &block|
+        method_name = "#{self.class}.#{method}"
+        args_list = args.join(',')
+        result = proxy.call(*args, &block)
+        Rails.logger.debug "#{method_name} called, args: (#{args_list}), result: #{result}"
+        result
+      end
     end
   end
 end
