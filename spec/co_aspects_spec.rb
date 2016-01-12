1eac82adf4f8634c16ccc581fa23d99965c9b0e9
Gaston Jorquera <gjorquera@gmail.com>
2016-01-12 12:29:35 -0300

    Cleaner version spec

--- spec/co_aspects_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-
-describe CoAspects do
-  it 'has a version number' do
-    expect(CoAspects::VERSION).not_to be nil
-  end
-end
---
+++ spec/unit/version_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe CoAspects::VERSION do
+  it { is_expected.not_to be(nil) }
+end
