358c962dd86311e8aaa616e4445139d5e346b737
Gaston Jorquera <gjorquera@gmail.com>
2016-01-14 11:47:19 -0300

    Add rescue and notify aspect documentation

--- lib/co_aspects/aspects/rescue_and_notify_aspect.rb
+++ lib/co_aspects/aspects/rescue_and_notify_aspect.rb
@@ -2,6 +2,38 @@ require 'newrelic_rpm'
 
 module CoAspects
   module Aspects
+    # Rescues any error and notifies NewRelic about it, without raising it
+    # again.
+    #
+    # Important: If CoAspects::Aspects::RescueAndNotifyAspect.enable_test_mode!
+    # is executed, then instead of calling NewRelic, it will raise the exception
+    # as if this aspect didn't exist. It's to be used by tests, to see
+    # exceptions being raised inside `_rescue_and_notify` annotated methods.
+    #
+    # Enabling test mode is not thread safe!
+    #
+    # Examples
+    #
+    #   class MyClass
+    #     aspects_annotations!
+    #
+    #     _rescue_and_notify
+    #     def perform_with_error
+    #       fail 'Error'
+    #     end
+    #
+    #     _rescue_and_notify
+    #     def perform_without_error
+    #       :success
+    #     end
+    #   end
+    #
+    #   MyClass.new.perform_with_error
+    #   # NewRelic::Agent.notify_error(...)
+    #   # => nil
+    #
+    #   MyClass.new.perform_without_error
+    #   # => :success
     class RescueAndNotifyAspect < Aspector::Base
       class << self
         def test_mode
