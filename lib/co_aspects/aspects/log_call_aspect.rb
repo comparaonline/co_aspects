a7c2ea75bc1e610225dd0a02823479633d52747e
Gaston Jorquera <gjorquera@gmail.com>
2016-01-14 11:33:16 -0300

    Add log_call documentation

--- lib/co_aspects/aspects/log_call_aspect.rb
+++ lib/co_aspects/aspects/log_call_aspect.rb
@@ -1,5 +1,21 @@
 module CoAspects
   module Aspects
+    # Generates a log in the Rails logger with the method call, arguments and
+    # result.
+    #
+    # Examples
+    #
+    #   class MyClass
+    #     aspects_annotations!
+    #
+    #     _log_call
+    #     def method(arg1, arg2)
+    #       :success
+    #     end
+    #   end
+    #
+    #   MyClass.new.method(:first, :second)
+    #   # => [DEBUG] MyClass.method called, args(:first,:second), result: :success
     class LogCallAspect < Aspector::Base
       around method_arg: true do |method, proxy, *args, &block|
         method_name = "#{self.class}.#{method}"
